a03d3fd82d89f9f49828d138d47be7062c87dffa
Hknyzgn <yzgnhakan@gmail.com>
2024-07-28 13:30:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from PIL import Image
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 # OpenAI API anahtarı
-openai.api_key = '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'
+openai.api_key = 'sk-proj-R0JCJAouk0SgTmMg0PsRT3BlbkFJo7atIv7CC7mzHXLURTGy'
 
 # Vektör mağazası ve asistan ID'leri
 vector_store_id = "vs_1N5yiB3FktibNA46NuqXrAVg"
